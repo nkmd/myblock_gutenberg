92a76d9e2c467875d9f3a5e053a84e9da50b57e5
Nikolay <nikolay.it.master@gmail.com>
2024-02-16 16:50:44 +0200

    dynamicblock 21

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'fea4a9f036e79436e2c3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '7aa19e27470dff411abe');
--- dynamicblock.php
+++ dynamicblock.php
@@ -7,7 +7,7 @@ Author: Nikolay I.
 Author URI:  http://nikolay.nk.md
  */
 
- function genius_myblock_init(){
+ function genius_dynamicblock_init(){
     register_block_type_from_metadata( __DIR__ );
  }
- add_action('init','genius_myblock_init');
+ add_action('init','genius_dynamicblock_init');
